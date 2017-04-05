5d8b30afdfcd9e73bdd9ab3e8092f54a3fa7c750
John Kayne <johnhoganuke2@gmail.com>
2017-04-05 12:56:33 -0400

    Update and rename CollisionMask.java to Factorio.java

---
+++ sorting/src/java/Factorio.java
@@ -0,0 +1,13 @@
+package factorioide.factorio;
+
+public class Factorio {
+  private String version;
+  
+  public String getVersion() {
+    return version;
+  }
+  
+  public static Factorio getFactorio() {
+    
+  }
+}
