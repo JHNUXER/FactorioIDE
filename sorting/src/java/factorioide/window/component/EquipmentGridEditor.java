215fc40c9ab5c2a4d12798ed7082eb410f7e3a5d
John Kayne <johnhoganuke2@gmail.com>
2017-04-05 14:26:25 -0400

    Update EquipmentGridEditor.java

--- sorting/src/java/factorioide/window/component/EquipmentGridEditor.java
+++ sorting/src/java/factorioide/window/component/EquipmentGridEditor.java
@@ -1,3 +1,4 @@
+
 public class EquipmentGridEditor extends JPanel {
   public JTextField nameInput;
   public JTextField orderInput;
@@ -9,8 +10,23 @@ public class EquipmentGridEditor extends JPanel {
     JLabel nameLabel = new JLabel("Name:");
     JLabel orderLabel = new JLabel("Order:");
     JLabel localizedNameLabel = new JLabel("Localized Name");
-    JLabel Size = new JLabel("Size:");
+    JLabel sizeLabel = new JLabel("Size:");
+    nameInput = new JTextField();
+    orderInput = new JTextField();
+    localizedNameInput = new JTextField();
+    widthInput = new JTextField();
+    height = new JTextField();
     
+    JPanel sizePanel = new JPanel();
+    JPanel labelColumn = new JPanel();
+    labelColumn.setLayout(new BoxLayout(labelColumn,BoxLayout.Y_AXIS));
+    JPanel inputColumn = new JPanel();
+    inputColumn.setLayout(new BoxLayout(inputColumn,BoxLayout.Y_AXIS));
+    sizePanel.add(widthInput,new JLabel("x"),heightInput);
+    labelColumn.add(nameLabel);
+    labelColumn.add(localizedNameLabel);
+    labelColumn.add(orderInput);
+    labelColumn.add(
   }
   
   public EquipmentGrid save() {
