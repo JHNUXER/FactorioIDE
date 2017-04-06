16fafcd29b4598e1107c8e4e4555a9df2e034201
John Kayne <johnhoganuke2@gmail.com>
2017-04-06 13:38:30 -0400

    Update Factorio.java

--- sorting/src/java/factorioide/factorio/Factorio.java
+++ sorting/src/java/factorioide/factorio/Factorio.java
@@ -1,4 +1,14 @@
+package factorioide.factorio;
+
+import factorioide.factorio.mod.Mod;
+
 public class Factorio {
   public Mod core;
   public Mod base;
+  
+  public Factorio(File) {
+    JSONObject json = (JSONObject) (new JSONParser().parse(file));
+    this.core = new Mod((JSONObject) json.getValue("core"));
+    this.base = new Mod((JSONObject) json.getValue("base"));
+  }
 }
